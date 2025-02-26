799e672200cf5ba066405a990f57ad2543d9c2dd
LenaBraunschweig <lena.braunschweig@gmail.com>
2025-02-25 18:47:54 -0600

    updated notes

--- 07-interpreters.rkt
+++ 07-interpreters.rkt
@@ -100,10 +100,13 @@ Review: What is parsing?
   our input to produce an initial syntax tree.
 -----------------------------------------------------------------------------|#
 
-;;; using Racket's reader
+;;; using Racket's reader- does the job of the parser
 #; (read)
 
+;;(read (open-input-string "(+ 1 2)"))
 
+(define (read-string str)
+  (read (open-input-string str)))
 
 #|-----------------------------------------------------------------------------
 ;; Parser (continued)
@@ -118,21 +121,41 @@ Review: What is parsing?
 ;;; Some types for decorating our syntax tree
 
 ;; integer value
-(struct int-exp () #:transparent)
+(struct int-exp (val) #:transparent)
 
-;; arithmetic expression
-(struct arith-exp () #:transparent)
+;; arithmetic expression (binary (two) addition or multiplication)
+(struct arith-exp (op lhs rhs) #:transparent)
 
 ;; variable
-(struct var-exp () #:transparent)
+(struct var-exp (id) #:transparent)
 
 ;; let expression
-(struct let-exp () #:transparent)
+(struct let-exp (ids vals body) #:transparent) ;; ids and vals are lists of things
 
 
-;; Parser
+;; Parser (recursive descent- creates the decorated syntax tree
 (define (parse sexp)
-  (void))
+  (match sexp
+    [(? integer?)
+     (int-exp sexp)]
+    
+    [(list '+ lhs rhs)
+     (arith-exp "ADD" (parse lhs) (parse rhs))]
+    [(list '* lhs rhs)
+     (arith-exp "TIMES" (parse lhs) (parse rhs))]
+    
+    [(? symbol?)
+     (var-exp sexp)]
+    
+    ;; ex. (let [x (* 2 5)] (+ x 1)
+    [(list 'let(list id exp) body)
+     (let-exp (list id) (parse (list exp)) (parse body))]
+
+    ;; the .. allows for multiple id exp lists
+    [(list 'let(list (list id exp) ...) body)
+     (let-exp id (map parse exp) (parse body))]))
+
+(parse (read-string p1))
 
 
 #|-----------------------------------------------------------------------------
@@ -142,5 +165,28 @@ Review: What is parsing?
 -----------------------------------------------------------------------------|#
 
 ;; Interpreter
-(define (eval expr)
-  (void))
+;; assoc returns the pair with the first item containing the item being searched for
+  ;; ex. assoc 'x '(x . 10) will return (x . 10)
+(define (eval expr [env '()])
+  (match expr
+    [(int-exp val)
+     val]
+    [(arith-exp "ADD" lhs rhs)
+     (+ (eval lhs env) (eval rhs env))]
+    [(arith-exp "TIMES" lhs rhs)
+     (* (eval lhs env) (eval rhs env))]
+    [(var-exp id)
+     (cdr (assoc id env))]
+    [(let-exp (list id) (list exp) body)
+    ;; first cons creating binding, while the second cons puts it in the new environment
+     (let ([nenv (cons (cons id (eval exp env)) env)])
+      (eval body nenv))]
+    [(let-exp (list id ...) (list exp ...) body)
+     (let ([nvars (map cons id (map (lambda (e) (eval e env)) exp))])
+      (eval body (append nvars env))]))
+
+;; creates an open to type sectiont that runs the eval after enter is pressed
+(define (repl)
+  (let ([ir (parse (read))])
+    (println (eval ir))
+    (repl)))
